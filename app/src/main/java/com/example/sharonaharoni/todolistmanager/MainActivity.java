6eaf113c6dcd09cf820a1c3a7922e7d5bc92db62
Matan <matan.modai@gmail.com>
2017-03-21 20:54:49 +0200

    Added the logic to add and remove items for list

--- app/src/main/java/com/example/sharonaharoni/todolistmanager/MainActivity.java
+++ app/src/main/java/com/example/sharonaharoni/todolistmanager/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.sharonaharoni.todolistmanager;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> listItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +36,55 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        /* Initialize list of items and adapter */
+        listItems = new ArrayList<>();
+        for (int i=0; i < 20; i++) {
+            listItems.add("item " + Integer.toString(i));
+        }
+        /* Override getView to alternate text color */
+        arrayAdapter = new ArrayAdapter<String>(this, R.layout.my_layout, listItems) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                TextView tv = (TextView) super.getView(position, convertView, parent);
+                if (position % 2 == 0) {
+                    tv.setTextColor(ContextCompat.getColor(this.getContext(), R.color.red));
+                } else {
+                    tv.setTextColor(ContextCompat.getColor(this.getContext(), R.color.blue));
+                }
+
+                return tv;
+            }
+        };
+
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(arrayAdapter);
+
+        /* Set long click listener */
+        listView.setLongClickable(true);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView parent, View view, final int position, long id) {
+                String selectedItem = ((TextView) view).getText().toString();
+                if (selectedItem.trim().equals(listItems.get(position).trim())) {
+                    removeElement(selectedItem, position);
+                }
+                return true;
+            }
+        });
+
+        /* Add element with FAB */
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                EditText editText = (EditText) findViewById(R.id.editText);
+                String newItem = editText.getText().toString();
+                listItems.add(newItem);
+                arrayAdapter.notifyDataSetChanged();
+                listView.setAdapter(arrayAdapter);
+                editText.setText("");
             }
         });
     }
@@ -48,5 +109,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void removeElement(String selectedItem, final int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Remove " + selectedItem + "?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                listItems.remove(position);
+                listView.setAdapter(arrayAdapter);
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
     }
 }
